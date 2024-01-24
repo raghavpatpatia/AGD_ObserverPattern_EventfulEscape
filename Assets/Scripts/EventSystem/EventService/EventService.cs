7616b00badb4503f17d0d8f6886d9c912a611076
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-01-24 15:47:46 +0530

    Horror-Escape Game
    Added EventService

---
+++ Assets/Scripts/EventSystem/EventService/EventService.cs
@@ -0,0 +1,22 @@
+﻿public class EventService
+{
+    public static EventService instance;
+    public static EventService Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new EventService();
+            }
+            return instance;
+        }
+    }
+
+    public EventController OnLightSwitchToggled { get; private set; }
+
+    public EventService()
+    {
+        OnLightSwitchToggled = new EventController();
+    }
+}
