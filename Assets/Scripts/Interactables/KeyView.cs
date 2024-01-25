ce1e0ec2dc1c9b26b35694ac116bc9de7c4c045a
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-01-25 12:20:33 +0530

    Horror-Escape Game
    Added OnKeysPickedUp

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -5,11 +5,12 @@ public class KeyView : MonoBehaviour, IInteractable
     [SerializeField] GameUIView gameUIView;
     public void Interact()
     {
+        int currentKeys = GameService.Instance.GetPlayerController().KeysEquipped;
         GameService.Instance.GetInstructionView().HideInstruction();
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
-        GameService.Instance.GetPlayerController().KeysEquipped++;
-        gameUIView.UpdateKeyText();
-
+        
+        currentKeys++;
+        EventService.Instance.OnKeyPickedUp.InvokeEvent(currentKeys);
         gameObject.SetActive(false);
     }
 }
