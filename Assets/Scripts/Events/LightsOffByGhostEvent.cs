47115da9d533d04d2cbb37d2c8ef11b79bd0b5f1
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-01-26 06:35:03 +0530

    Horror-Escape Game
    Added OnLightsTurnOffByGhost event.

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
+    [SerializeField] private int keysRequired;
+    [SerializeField] private SoundType soundType;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerView>() != null && keysRequired == GameService.Instance.GetPlayerController().KeysEquipped)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
+            GetComponent<Collider>().enabled = false;
+        }
+    }
 }
